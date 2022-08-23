9c46fb590aeefd398ae4aa01ebeba732f2935f6b
MARTA <martariosrguez@hotmail.com>
2022-08-23 17:08:36 +0200

    Completed

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jbuilder', '~> 2.11'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1'
+gem 'rails_admin', '~> 2.0'
 
 # AWS S3
 gem 'aws-sdk-s3', '~> 1.112'
@@ -44,6 +45,7 @@ gem 'bootsnap', '>= 1.10.3', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'rspec-rails', '~> 4.0'
   gem 'byebug', '~> 11.1.3', platforms: [:mri, :mingw, :x64_mingw]
 
   # Added by Altcademy.com
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'homepage#index'
 
-  # Add routes below this line
-
-
-
-
-
-  # Add routes below above line
+  get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
+  delete 'tasks/:id' => 'tasks#destroy'
+  put 'tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put 'tasks/:id/mark_active' => 'tasks#mark_active'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path'    => 'homepage#index'
